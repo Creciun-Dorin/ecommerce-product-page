c9d58244aa9fdd0719b789e854b4abc15b8e0242
Creciun-Dorin <creciundorin5@gmail.com>
2023-11-07 13:23:51 +0200

    am modificat fisierul css

--- scripts/module/dataBD.js
+++ scripts/module/dataBD.js
@@ -1,6 +1,6 @@
 
 async function dataBD(){
-    const response = await fetch('https://github.com/Creciun-Dorin/Creciun-Dorin-ecommerce-product-page/blob/main/dataBD.json');
+    const response = await fetch('../../db.json');
     const data = await response.json();
     return data;
 }
