de174bcf9dfc262894a1efc5b952d0cfa1240467
Creciun-Dorin <creciundorin5@gmail.com>
2023-11-07 13:31:58 +0200

    am modificat fisierul css

--- scripts/module/dataBD.js
+++ scripts/module/dataBD.js
@@ -1,6 +1,6 @@
 
 async function dataBD(){
-    const response = await fetch('/blob/main/db.json');
+    const response = await fetch('../../db.json');
     const data = await response.json();
     return data;
 }
